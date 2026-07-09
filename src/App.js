541f72dda194fdd913280aa91f4c92c5526ce6b1
Akshay-Do <akshaydodhia02@gmail.com>
2026-07-09 19:07:16 +0300

    Update to App.js added route to Add listing and App.css added styling for Add listing  page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import FindHousing from './Pages/FindHousing/FindHousing';
 import LandlordDashboard from './Pages/LandlordPortal/LandlordPortal';
+import AddListing from './Pages/LandlordPortal/AddListing';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/findhousing" exact={true} element={<FindHousing />} />
         <Route path="/manageproperties" exact={true} element={<LandlordDashboard />} />
         <Route path="/dashboard" exact={true} element={<LandlordDashboard />} />
+        <Route path="/add-listing" exact={true} element={<AddListing />} />
       </Routes>
       <Footer />
     </BrowserRouter>
